ef4c5e0dbdafdb9097dd8e616a3bcc379185bc54
MaximilChristian <50239793+MaximilChristian@users.noreply.github.com>
2021-07-15 12:07:32 +0700

    update

---
+++ application/views/customer/dashboard.php
@@ -0,0 +1,310 @@
+<!-- Page Content -->
+<!-- Banner Starts Here -->
+<div class="main-banner header-text">
+  <div class="container-fluid">
+    <div class="owl-banner owl-carousel">
+      <div class="item">
+        <img src="<?= base_url('assets/assets_shop') ?>/images/forkliftkom2.jpg" alt="">
+        <div class="item-content">
+          <div class="main-content">
+            <div class="meta-category">
+              <span>
+                <i class="fa fa-user"></i> 1 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-briefcase"></i> 1 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-sign-out"></i> 1 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-cog"></i> B
+              </span>
+            </div>
+            <a href="fleet.html"><h4>Forklift Komatsu.</h4></a>
+            <ul class="post-info">
+              <li>Produk Forklift Berkualitas dari Komatsu dengan kapasitas 3 Ton.</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="item">
+        <img src="<?= base_url('assets/assets_shop') ?>/images/forklifttcm.jpg" alt="">
+        <div class="item-content">
+          <div class="main-content">
+            <div class="meta-category">
+              <span>
+                <i class="fa fa-user"></i> 1 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-briefcase"></i> 1 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-sign-out"></i> 1 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-cog"></i> B
+              </span>
+            </div>
+            <a href="fleet.html"><h4>Forklift TCM.</h4></a>
+            <ul class="post-info">
+              <li>Forklift Berkualitas dengan kapasitas 3 Ton.</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="item">
+        <img src="<?= base_url('assets/assets_shop') ?>/images/forklift.jpg" alt="">
+        <div class="item-content">
+          <div class="main-content">
+            <div class="meta-category">
+              <span>
+                <i class="fa fa-user"></i> 5 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-briefcase"></i> 4 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-sign-out"></i> 4 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-cog"></i> A
+              </span>
+            </div>
+            <a href="fleet.html"><h4>Forklift TCM.</h4></a>
+            <ul class="post-info">
+              <li>Forklift Berkualitas dengan kapasitas 7 Ton.</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="item">
+        <img src="<?= base_url('assets/assets_shop') ?>/images/forklift10.jpg" alt="">
+        <div class="item-content">
+          <div class="main-content">
+            <div class="meta-category">
+              <span>
+                <i class="fa fa-user"></i> 5 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-briefcase"></i> 4 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-sign-out"></i> 4 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-cog"></i> A
+              </span>
+            </div>
+            <a href="fleet.html"><h4>Forklift TCM</h4></a>
+            <ul class="post-info">
+              <li>Forklift TCM berkualitas dengan Kapasitas 10 Ton</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="item">
+        <img src="<?= base_url('assets/assets_shop') ?>/images/forklift4.png" alt="">
+        <div class="item-content">
+          <div class="main-content">
+            <div class="meta-category">
+              <span>
+                <i class="fa fa-user"></i> 5 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-briefcase"></i> 4 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-sign-out"></i> 4 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-cog"></i> A
+              </span>
+            </div>
+            <a href="fleet.html"><h4>Forklift Komatsu</h4></a>
+            <ul class="post-info">
+              <li>Forklift Komatsu Berkualitas dengan Kapasitas 10 Ton</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="item">
+        <img src="<?= base_url('assets/assets_shop') ?>/images/forkliftkom1.jpg" alt="">
+        <div class="item-content">
+          <div class="main-content">
+            <div class="meta-category">
+              <span>
+                <i class="fa fa-user"></i> 5 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-briefcase"></i> 4 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-sign-out"></i> 4 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-cog"></i> A
+              </span>
+            </div>
+            <a href="fleet.html"><h4>Forklift Komatsu</h4></a>
+            <ul class="post-info">
+              <li>Forklift Komatsu Berkualitas dengan Kapasitas 1,5 Ton</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Banner Ends Here -->
+
+<section class="blog-posts grid-system">
+  <div class="container">
+    <div class="all-blog-posts">
+      <h2 class="text-center">Promo</h2>
+      <br>
+      <div class="row">
+        <div class="col-md-4 col-sm-6">
+          <div class="blog-post">
+            <div class="blog-thumb">
+              <img src="<?= base_url('assets/assets_shop') ?>/images/forklift2.jpg" alt="">
+            </div>
+            <div class="down-content">
+              <strong>Potongan</strong> <span> 100.000 </span> <strong>diatas 5 jam penyewaan</strong>
+              <a href="offers.html"><h4>Promo ini berlaku untuk penyewaan Forklift 7 Ton</h4></a>
+              <div class="post-options">
+                <div class="row">
+                  <div class="col-lg-12">
+                    <ul class="post-tags">
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 col-sm-6">
+          <div class="blog-post">
+            <div class="blog-thumb">
+              <img src="<?= base_url('assets/assets_shop') ?>/images/forkliftkom.jpg" alt="">
+            </div>
+            <div class="down-content">
+              <strong>Gratis</strong> <span> 1 Jam </span> <strong>diatas 4 jam penyewaan</strong>
+              <a href="offers.html"><h4>Promo ini Berlaku untuk Penyewaan Forklift 10 Ton</h4></a>
+              <div class="post-options">
+                <div class="row">
+                  <div class="col-lg-12">
+                    <ul class="post-tags">
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 col-sm-6">
+          <div class="blog-post">
+            <div class="blog-thumb">
+              <img src="<?= base_url('assets/assets_shop') ?>/images/forklift3.jpg" alt="">
+            </div>
+            <div class="down-content">
+              <strong>Gratis</strong> <span> 1 Jam </span> <strong>Setiap 20 Jam Penyewaan</strong>
+              <a href="offers.html"><h4>Promo ini Berlaku untuk penyewaan Forklift Diatas 3 Ton </h4></a>
+              <div class="post-options">
+                <div class="row">
+                  <div class="col-lg-12">
+                    <ul class="post-tags">
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="call-to-action">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12">
+        <div class="main-content">
+          <div class="row">
+            <div class="col-lg-8">
+              <h4>Anda Berminat sewa Forklift? Silahkan Hubungi Kami</h4>
+            </div>
+            <div class="col-lg-4">
+              <div class="main-button">
+                <a href="contact.html">Contact Us</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="blog-posts grid-system">
+  <div class="container">
+    <div class="all-blog-posts">
+      <h2 class="text-center">Area Layanan</h2>
+      <br>
+      <div class="row">
+        <div class="col-md-4 col-sm-6">
+          <div class="blog-post">
+            <div class="blog-thumb">
+            </div>
+            <div class="down-content">
+              <p>Kamal Muara dan sekitarnya</p>
+							<p>Tegal Alur dan sekitarnya</p>
+							<p>Cengkareng dan sekitarnya</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 col-sm-6">
+          <div class="blog-post">
+            <div class="blog-thumb">
+            </div>
+            <div class="down-content">  
+              <p>Kapuk dan Sekitarnya</p>
+							<p>PIK dan Sekitarnya</p>
+							<p>Dadap dan Sekitarnya</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 col-sm-6">
+          <div class="blog-post">
+            <div class="blog-thumb">
+            </div>
+            <div class="down-content">
+              <p>Rawa Bokor dan Sekitarnya</p>
+							<p>Palem dan sekitarnya</p>
+							<p>Citra dan Sekitarnya</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<div class="blog-posts">
+  <div class="container">
+    <div class="All-blog-posts">
+        <h2 class="text-center">About Us</h2>
+      <br>
+      <br>
+      <div class="content">
+        <ul>
+          <li>
+            <div class="author-thumb">
+              <img src="<?= base_url('assets/assets_shop') ?>/images/comment-author-01.jpg" alt="">
+            </div>
+            <div class="right-content">
+              <h4>John Doe<span>10.07.2020</span></h4>
+              <p>Fusce ornare mollis eros. Duis et diam vitae justo fringilla condimentum eu quis leo. Vestibulum id turpis porttitor sapien facilisis scelerisque. Curabitur a nisl eu lacus convallis eleifend posuere id tellus.</p>
+            </div>
+          </li>
+          <li>
+            <div class="author-thumb">
+              <img src="<?= base_url('assets/assets_shop') ?>/images/comment-author-02.jpg" alt="">
+            </div>
+            <div class="right-content">
+              <h4>Jane Smith<span>10.07.2020</span></h4>
+              <p>Nullam nec pharetra nibh. Cras tortor nulla, faucibus id tincidunt in, ultrices eget ligula. Sed vitae suscipit ligula. Vestibulum id turpis volutpat, lobortis turpis ac, molestie nibh.</p>
+            </div>
+          </li>
+          <li>
+            <div class="author-thumb">
+              <img src="<?= base_url('assets/assets_shop') ?>/images/comment-author-03.jpg" alt="">
+            </div>
+            <div class="right-content">
+              <h4>Kate Blue<span>10.07.2020</span></h4>
+              <p>Nullam nec pharetra nibh. Cras tortor nulla, faucibus id tincidunt in, ultrices eget ligula. Sed vitae suscipit ligula. Vestibulum id turpis volutpat, lobortis turpis ac, molestie nibh.</p>
+            </div>
+          </li>
+        </ul>
+      </div>
+
+      <br>
+      <br>
+    
+      <div class="row justify-content-md-center">
+        <div class="col-md-3">
+          <div class="main-button">
+            <a href="testimonials.html">Read More</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div style="height: 180px;"></div>
